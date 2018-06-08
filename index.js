8b0be8301e0c76207568de1f774e27c8c7fc1858
Adalberto Dos Remedios Silva Junior <ajuni46@Nextels-MacBook-Pro.local>
2018-06-08 11:54:26 -0300

    - ajustes de configuração do projeto

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./config/app')
+
+const PORT = parseInt(process.env.PORT, 10) || 3000
+app.set('port', PORT)
+
+app.listen(PORT)
+
+console.log(`-------------------------------------  Server up on port ${PORT} -------------------------------------`)
