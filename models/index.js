1d859937e41f24b6c5b1c62593785988fcc0cb43
adalbertorsilva <adalbertorsilvajr@gmail.com>
2018-06-13 02:47:30 -0300

    - ajustes de deploy

--- models/index.js
+++ models/index.js
@@ -6,7 +6,6 @@ const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
 const config = require(`../config/config.js`)[env]
-require('dotenv').config()
 const db = {}
 
 let sequelize
